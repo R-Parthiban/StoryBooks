f959fe58bdb8985886d06e8b3c574f22d924eba1
R-Parthiban <parthi1996@gmail.com>
2018-11-07 20:11:59 +0530

    version 1.0.0 alpha

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const path = require("path");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 const session = require("express-session");
 const passport = require("passport");
+
 const keys = require("./config/keys");
 
 //Load user  model
 require("./models/user");
+require("./models/Story");
 
 //Passport  config
 require("./config/passport")(passport);
@@ -15,8 +20,18 @@ require("./config/passport")(passport);
 //Load routes
 const auth = require("./routes/auth");
 const index = require("./routes/index");
+const stories = require("./routes/stories");
 
 const app = express();
+
+//Handlebars helpers
+const {
+  truncate,
+  stripTags,
+  formatDate,
+  select,
+  editIcon
+} = require("./helpers/hbs");
 
 //Mongoose connect
 mongoose
@@ -29,10 +44,24 @@ mongoose
   .then(() => console.log("MongoDb connected.."))
   .catch(err => console.log(err));
 
+//BodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Method Override middleware
+app.use(methodOverride("_method"));
+
 //Handlebar middleware
 app.engine(
   "handlebars",
   exphbs({
+    helpers: {
+      truncate: truncate,
+      stripTags: stripTags,
+      formatDate: formatDate,
+      select: select,
+      editIcon: editIcon
+    },
     defaultLayout: "main"
   })
 );
@@ -57,8 +86,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, "public")));
+
 //use routes
 app.use("/auth", auth);
+app.use("/stories", stories);
 app.use("/", index);
 
 const port = process.env.PORT || 5000;
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://parthi:parthi147@ds151293.mlab.com:51293/storybooks-dev",
-  googleClientID:
-    "360748583082-tiff6mn5bbc4qolhpmneh3o14dogbltj.apps.googleusercontent.com",
-  googleClientSecret: "fH1kJ58_hlNgNtlU3noR6Z7K"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}

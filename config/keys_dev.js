2b7eeae7951e8b592a83aaf93e42a564bde4e498
R-Parthiban <parthi1996@gmail.com>
2018-11-08 20:48:08 +0530

    adding keys_dev file

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://parthi:parthi147@ds151293.mlab.com:51293/storybooks-dev",
+  googleClientID:
+    "360748583082-tiff6mn5bbc4qolhpmneh3o14dogbltj.apps.googleusercontent.com",
+  googleClientSecret: "fH1kJ58_hlNgNtlU3noR6Z7K"
+};
